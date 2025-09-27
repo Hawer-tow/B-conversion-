ed27f1c01b9bc6fe2dd74460cf0dd031efb529b4
Hawer-tow <TNTHAWER1@GMAIL.COM>
2025-09-27 09:08:11 +0300

    Update D2B_by2.java

--- D2B_by2.java
+++ D2B_by2.java
@@ -1,92 +1,87 @@
+package com.example.myconversions;
+
 // D2B_by2
 
 public class D2B_by2{
 
 
-  
-    static Stack mystack = new Stack(32);
-    static Queue myqueue = new Queue(20);
+
+    //static Stack mystack = new Stack(32);
+    //static Queue myqueue = new Queue(20);
 
 
-public static void divide(int a)
-{
-        
-    int value = a;
-  
+    public static void divide(int a , Stack mystack)
+    {
 
-         if (value == 0)
-            {
-             return ;
-            }
-        int remainder = value%2;
-        int pass = (value-remainder)/2;
-        mystack.push(remainder);
-        divide(pass);
-}  
-public static void printstack(int a)
-{       
-    //System.out.println("The binary equivalent of " + a + " is :");
-     while (!mystack.isEmpty())
-            {
-            
+        int value = a;
+
+
+        if (value == 0)
+        {
+            return ;
+        }
+        else {
+            int remainder = value % 2;
+            int pass = (value - remainder) / 2;
+            mystack.push(remainder);
+            divide(pass, mystack);
+        }
+    }
+    public static void printstack(int a , Stack mystack)
+    {
+        //System.out.println("The binary equivalent of " + a + " is :");
+        while (!mystack.isEmpty())
+        {
+
             System.out.print( mystack.pop() +"");
-            }
-           // System.out.println();
-}
+        }
+        // System.out.println();
+    }
 
 
 
-   // fractional part of the double decimal.
+    // fractional part of the double decimal.
 
-public static void mupltiply(double a){
-double value = a;
-double fracprodt= value * 2;
+    public static void mupltiply(double a , Queue myqueue){
+        double value = a;
+        double fracprodt= value * 2;
         if(fracprodt == 0)
         {
             return;
         }
-
-        double pass = fracprodt - (int)fracprodt;
-        myqueue.enqueue((int)fracprodt);
-        mupltiply(pass);
-
-
-
-}
-public static void printqueue(double a){
-      //System.out.println("The binary equivalent of " + a + " is :");
-         
-     myqueue.display();
-    
-        System.out.println(); 
-} 
+        else {
+            double pass = fracprodt - (int) fracprodt;
+            myqueue.enqueue((int) fracprodt);
+            mupltiply(pass, myqueue);
+        }
 
 
-public static void main(String[] args){
+    }
+    public static void printqueue(double a , Queue myqueue){
+        //System.out.println("The binary equivalent of " + a + " is :");
+        if (!myqueue.isEmpty())
+        {
 
- double decimal = 12.3125 ;
- 
+            myqueue.display();
+
+        }
+
+        System.out.println();
+    }
 
 
-double integerPart = Math.floor(decimal);
-double fractionalPart = decimal - integerPart;
-
-divide((int)integerPart);
-mupltiply(fractionalPart);
+    public static void main(String[] args){
 
 
 
-System.out.println("The Binary equivalent of " + decimal + " is :");
-printstack((int)integerPart);
-printqueue(fractionalPart);
 
- 
-  
 
+
+
+    }
 
 }
 
-}
 
 
 
