c4722b8fe21e46b5e8263c1e1772592be4a2bba5
Hawer-tow <TNTHAWER1@GMAIL.COM>
2025-09-27 09:17:25 +0300

    Update O2Hex.java

--- O2Hex.java
+++ O2Hex.java
@@ -1,3 +1,5 @@
+package com.example.myconversions;
+
 // O2Hex
 
 import java.util.ArrayList;
@@ -6,16 +8,16 @@ import java.util.Collections;
 
 public class O2Hex {
 
-  static ArrayList<String> chunks = new ArrayList<>();
-  static ArrayList<String> groups = new ArrayList<>();
-  static B2Hex binary2hex = new B2Hex();
+//    static ArrayList<String> chunks = new ArrayList<>();
+//    static ArrayList<String> groups = new ArrayList<>();
 
-public static void inthex(String a)
-{
-    String value = a;
-    String finalout ="";
 
-       // Split from the end into groups of 4
+    public static void inthex(String a ,ArrayList<String> groups,ArrayList<String> groups2 , Queue2 myqueue1)
+    {
+        String value = a;
+        String finalout ="";
+
+        // Split from the end into groups of 4
         for (int i = value.length(); i > 0; i -= 1) {
             int start = Math.max(i - 1, 0);
             String chunk = value.substring(start, i);
@@ -34,154 +36,140 @@ public static void inthex(String a)
         // Output the result
         //System.out.println(groups);
         for (int i = 0; i < groups.size(); i++)
-         {
-             //System.out.println(groups.get(i));
-               
-             String x = groups.get(i);
+        {
+            //System.out.println(groups.get(i));
+
+            String x = groups.get(i);
             // System.out.println(x + x.getClass().getSimpleName() );
 
-if (x.equals("0"))
-{
-     String y = "000";
-     finalout += y;
-}
-else if (x.equals("1"))
-{
-     String y ="001";
-     finalout += y;
-}
-else if (x.equals("2"))
-{
-    String y ="010";
-     finalout += y;
-}
-else if (x.equals("3"))
-{
-     String y ="011";
-     finalout += y;
-}
-else if (x.equals("4"))
-{
-     String y ="100";
-     finalout += y;
-}
-else if (x.equals("5"))
-{
-     String y ="101";
-     finalout += y;
-}
-else if (x.equals("6"))
-{
-     String y ="110";
-     finalout += y;
-}
-else if (x.equals("7"))
-{
-   String y ="111";
-     finalout += y;
-}
+            if (x.equals("0"))
+            {
+                String y = "000";
+                finalout += y;
+            }
+            else if (x.equals("1"))
+            {
+                String y ="001";
+                finalout += y;
+            }
+            else if (x.equals("2"))
+            {
+                String y ="010";
+                finalout += y;
+            }
+            else if (x.equals("3"))
+            {
+                String y ="011";
+                finalout += y;
+            }
+            else if (x.equals("4"))
+            {
+                String y ="100";
+                finalout += y;
+            }
+            else if (x.equals("5"))
+            {
+                String y ="101";
+                finalout += y;
+            }
+            else if (x.equals("6"))
+            {
+                String y ="110";
+                finalout += y;
+            }
+            else if (x.equals("7"))
+            {
+                String y ="111";
+                finalout += y;
+            }
 
 //System.out.println(finalout);
-}
-binary2hex.groupings(finalout);
-}
+        }
+        B2Hex.groupings(finalout,groups2, myqueue1);
+    }
 
 
 
-public static void frachex(String a){
+    public static void frachex(String a ,ArrayList<String> chunks ,ArrayList<String> chunks2 ,Queue2 myqueue2){
 
-    String value = a;
-   String finalout= "";
+        String value = a;
+        String finalout= "";
 
 // Pad to make length a multiple of 4
-int padLength = 1 - (value.length() % 1);
-if (padLength != 1) {
-    value = value + "0".repeat(padLength); // Pad at the beginning
-}
+        int padLength = 1 - (value.length() % 1);
+        if (padLength != 1) {
+            value = value + "0".repeat(padLength); // Pad at the beginning
+        }
 
-for (int i = 0; i < value.length(); i += 1) {
-    chunks.add(value.substring(i ,i +1));
-}
+        for (int i = 0; i < value.length(); i += 1) {
+            chunks.add(value.substring(i ,i +1));
+        }
 
-  //System.out.println(chunks); // Output: [1011, 1000]
+        //System.out.println(chunks); // Output: [1011, 1000]
 
-       
+
         for (int i = 0; i < chunks.size(); i++)
-         {
-             //System.out.println(chunks.get(i));
-             String x = chunks.get(i);
+        {
+            //System.out.println(chunks.get(i));
+            String x = chunks.get(i);
             // System.out.println(x + x.getClass().getSimpleName() );
 
-if (x.equals("0"))
-{
-     String y = "000";
-     finalout += y;
-}
-else if (x.equals("1"))
-{
-     String y ="001";
-     finalout += y;
-}
-else if (x.equals("2"))
-{
-    String y ="010";
-     finalout += y;
-}
-else if (x.equals("3"))
-{
-     String y ="011";
-     finalout += y;
-}
-else if (x.equals("4"))
-{
-     String y ="100";
-     finalout += y;
-}
-else if (x.equals("5"))
-{
-     String y ="101";
-     finalout += y;
-}
-else if (x.equals("6"))
-{
-     String y ="110";
-     finalout += y;
-}
-else if (x.equals("7"))
-{
-   String y ="111";
-     finalout += y;
-}
+            if (x.equals("0"))
+            {
+                String y = "000";
+                finalout += y;
+            }
+            else if (x.equals("1"))
+            {
+                String y ="001";
+                finalout += y;
+            }
+            else if (x.equals("2"))
+            {
+                String y ="010";
+                finalout += y;
+            }
+            else if (x.equals("3"))
+            {
+                String y ="011";
+                finalout += y;
+            }
+            else if (x.equals("4"))
+            {
+                String y ="100";
+                finalout += y;
+            }
+            else if (x.equals("5"))
+            {
+                String y ="101";
+                finalout += y;
+            }
+            else if (x.equals("6"))
+            {
+                String y ="110";
+                finalout += y;
+            }
+            else if (x.equals("7"))
+            {
+                String y ="111";
+                finalout += y;
+            }
 
 //System.out.println(finalout);
 
 
-} 
-binary2hex.fracgroupings(finalout);
-}
+        }
+        B2Hex.fracgroupings(finalout,chunks2,myqueue2);
+    }
 
 
 
     public static void main(String[] args) {
 
-        ArrayList<String> fracChunks = new ArrayList<>();
-
-        String octal = "321.0";
-        String[] parts = octal.split("\\.");
-
-        String intPart = parts[0];       // "10111"
-        String fracPart = parts[1]; 
-
-        inthex(parts[0]);
-        frachex(parts[1]);
-
-        System.out.println("The Hexadecimal equivalent of " + octal + " is : ");
-        binary2hex.printhex(parts[0]);
-        System.out.print(".");
-        binary2hex.printfrachex(parts[1]);
-
-        System.out.println("\n");
 
 
+
+
+    }
 }
-}
+
