49fdb4f71359b577693e8a6ad51f2dd12dcd8286
Hawer-tow <TNTHAWER1@GMAIL.COM>
2025-09-27 09:14:01 +0300

    Update D2Hex.java

--- D2Hex.java
+++ D2Hex.java
@@ -1,43 +1,43 @@
+package com.example.myconversions;
+
 // D2Hex uses stack and queue2;
 
 
 public class D2Hex {
 
 
-  
-    static Stack mystack = new Stack(30);
-    static Queue2 myqueue = new Queue2(20);
+
+    //static Stack mystack = new Stack(30);
+    //static Queue2 myqueue = new Queue2(20);
 
 
-public static void divide(int a)
-{
-        
-    int value = a;  
+    public static void divide(int a , Stack mystack)
+    {
 
-         if (value == 0)
-            {
-             return ;
-            }
+        int value = a;
+
+        if (value == 0)
+        {
+            return ;
+        }
         int remainder = value%16;
-       // System.out.println(remainder);
-         mystack.push(remainder);
+        // System.out.println(remainder);
+        mystack.push(remainder);
         int pass = (value-remainder)/16;
-       
-        divide(pass);
-}  
-public static void printstack(int a)
-{ 
 
-    //System.out.println("The binary equivalent of " + a + " is :");
-    
-    
+        divide(pass,mystack);
+    }
+    public static void printstack(int a , Stack mystack)
+    {
 
-     while (!mystack.isEmpty())
-                   {
-                    //System.out.print(mystack.pop() +"");
-                    int y = mystack.pop();
-                             
-             
+        //System.out.println("The binary equivalent of " + a + " is :");
+
+        while (!mystack.isEmpty())
+        {
+            //System.out.print(mystack.pop() +"");
+            int y = mystack.pop();
+
+
             if( y == 10)
             {
                 System.out.print("A");
@@ -61,26 +61,26 @@ public static void printstack(int a)
             else if (y == 15)
             {
                 System.out.print("F");
-            } 
+            }
             else
             {
 
                 System.out.print(y);
 
-            } 
-            
             }
-              
 
-}
-
+        }
 
 
-   // fractional part of the double decimal.
+    }
 
-public static void mupltiply(double a){
-double value = a;
-double fracprodt= value * 16;
+
+
+    // fractional part of the double decimal.
+
+    public static void mupltiply(double a , Queue2 myqueue){
+        double value = a;
+        double fracprodt= value * 16;
         if(fracprodt == 0)
         {
             return;
@@ -88,51 +88,35 @@ double fracprodt= value * 16;
 
         double pass = fracprodt - (int)fracprodt;
         myqueue.enqueue((int)fracprodt);
-        mupltiply(pass);
+        mupltiply(pass,myqueue);
 
 
 
-}
-public static void printqueue(double a){
-      //System.out.println("The binary equivalent of " + a + " is :");
-         
-     myqueue.display();
-    
-        System.out.print(""); 
-} 
+    }
+    public static void printqueue(double a , Queue2 myqueue){
+        //System.out.println("The binary equivalent of " + a + " is :");
+
+        myqueue.display();
+
+        System.out.print("");
+    }
 
 
-public static void main(String[] args){
-
- double decimal = 229.125 ;
- 
-
-
-double integerPart = Math.floor(decimal);
-double fractionalPart = decimal - integerPart;
+    public static void main(String[] args){
 
 
 
 
 
-divide((int)integerPart);
-mupltiply(fractionalPart);
 
 
 
-System.out.println("The Hexadecimal equivalent of " + decimal + " is : \n");
-printstack((int)integerPart);
-System.out.print(".");
-printqueue(fractionalPart);
 
-System.out.println("\n");
- 
-  
 
+    }
 
 }
 
-}
 
 
 
